7b90c8c896f4057a555c1f16fb17cb67af2a2492
Najah <najahmatsuo7@gmail.com>
2022-07-09 15:27:13 +0900

    モデル作成

--- myblog/api/models.py
+++ myblog/api/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Articles(models.Model):
+    """記事のモデル"""
+
+    class Meta:
+        db_table = 'article'
+        ordering = ['created_at']
+        verbose_name = verbose_name_plural = '記事'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    title = models.CharField(verbose_name='タイトル', max_length=20, unique=True)
+    text = models.TextField(verbose_name='本文', blank=False)
+    img = models.ImageField(verbose_name='画像', null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+
+
+class Genre(models.Model):
+    """ジャンルのモデル"""
+    class Meta:
+        db_table = 'genre'
+        ordering = ['id']
+        verbose_name = verbose_name_plural = 'ジャンル'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    genre_name = models.CharField(verbose_name='ジャンル名', max_length=30, unique=True)
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -121,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+if DEBUG:
+    MEDIA_ROOT = BASE_DIR/'media'
+else:
+    MEDIA_ROOT = f'/var/www/{BASE_DIR.name}/media'
